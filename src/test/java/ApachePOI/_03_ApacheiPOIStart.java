a45e51700a72a4ea60d9fb5f08b712d8a5d344c1
iuboyraz <iuboyraz@gmail.com>
2023-10-11 19:01:08 +0300

    updates

--- src/test/java/ApachePOI/_03_ApacheiPOIStart.java
+++ src/test/java/ApachePOI/_03_ApacheiPOIStart.java
@@ -14,24 +14,25 @@ import java.lang.reflect.Field;
 public class _03_ApacheiPOIStart {
 
     public static void main(String[] args) throws IOException {
-        // dosyanýn olduðu yerle program (burada IntellijIDE) arasýnda bir doya yolu baðlantýsý gerekli.
+        // dosyanýn olduðu yerle program (burada IntellijIDE) arasýnda gerekli olan dosya yolu baðlantýsýný tanýmlýyorum.
         String path = "src/test/java/ApachePOI/ExcelFiles/ApacheExcel2.xlsx";
 
-        // Java dosya okuma nesnesine bu yolu vererek baðlantý kuruyorum.
+        // Java dosya okuma nesnesine (dosyaOkumaBaglantisi) bu yolu vererek baðlantý kuruyorum.
         // Tek yönlü yani okuma olarak çalýþýyor.
         FileInputStream dosyaOkumaBaglantisi = new FileInputStream(path);
 
-        // Dosya okuma baðlantýsý üzerinden çalýþma kitabýný alýyorum.
+        // Dosya okuma baðlantýsý üzerinden çalýþma kitabýna ulaþýyorum.
         Workbook calismaKitabi = WorkbookFactory.create(dosyaOkumaBaglantisi);
 
-        // Istenen isimdeki calisma sayfasini aliyorum.
+        // Istenen isimdeki calisma sayfasina ulaþýyorum.
         Sheet calismaSayfasi = calismaKitabi.getSheet("Sheet1");
+        // Sheet calismaSayfasi = calismaKitabi.getSheetAt(0); // index ile ulaþma
 
-        // Istenen satiri aliyorum.
-        Row satir = calismaSayfasi.getRow(0); // 0. satiri aldým.
+        // Istenen satira ulaþýyorum.
+        Row satir = calismaSayfasi.getRow(0); // 0. satira ulaþtým.
 
-        // Istenen hucreyi aliyorum.
-        Cell hucre = satir.getCell(0); // Satirin 0. hucresini aldim.
+        // Istenen hucreye ulaþýyorum.
+        Cell hucre = satir.getCell(0); // Satirin 0. hucresine ulaþtým.
 
         System.out.println("hucre = " + hucre); // hucre = Lion
 
