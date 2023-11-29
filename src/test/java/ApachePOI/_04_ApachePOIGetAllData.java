4a8f9618f85a6000095f70b91dd86fada8ddda95
iuboyraz <iuboyraz@gmail.com>
2023-11-29 15:12:30 +0300

    Jenkins için _05_TestRunnerRegressionA JSON report eklentisi

--- src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
+++ src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class _04_ApachePOIGetAllData {
-
     public static void main(String[] args) throws IOException {
 
         String path = "src/test/java/ApachePOI/ExcelFiles/ApacheExcel2.xlsx";
